64eac8a4ee72e3f71c548f909e174333f9fbb0b0
Jvad <jvadkhan@gmail.com>
2020-06-06 14:09:55 -0700

    Update App.js
    Removed target for anchor tag

--- src/App.js
+++ src/App.js
@@ -16,20 +16,17 @@ function App() {
       <hr></hr>
       <ol>
         <li>
-          <a href="https://github.com/jvad/Project02.git" target="_blank">
-            My Repository
-          </a>
+          <a href="https://github.com/jvad/Project02.git">My Repository</a>
         </li>
         <li>
-          <a href="https://www.facebook.com/jvadii" target="_blank">
-            Contact Me
-          </a>
+          <a href="https://www.facebook.com/jvadii">Contact Me</a>
         </li>
       </ol>
       <img
         src="https://images.unsplash.com/photo-1590611936760-eeb9bc598548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
         height="300px"
         width="500px"
+        alt="Girl Wearing A Mask"
       />
     </div>
   );
